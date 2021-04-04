0f1bf949ee4c4805e95eb6f1d451b5b90b7ea4d8
Jash Shah <47540320+Jash-2000@users.noreply.github.com>
2021-04-04 18:03:33 +0530

    Added New Project

---
+++ New Unity Project 1/Assets/Coding/Main.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    Rigidbody player;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	    player = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+    	float hori = Input.GetAxis("Horizontal");
+    	float verti = Input.GetAxis("Vertical");
+
+    	Vector3 movement = new Vector3 (hori,0.0f,verti); // This is to define a new datatype for directions.
+    	player.AddForce(movement * speed);
+    }
+}
